a135724a4147636bb3356ec5bb38b3f3610f9b47
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-06-24 07:05:37 -0400

    Update connect.ts

--- src/main/adapters/connect.ts
+++ src/main/adapters/connect.ts
@@ -1,26 +1,3 @@
-Skip to content
-LucasHRTeam
-/
-gravity-ts
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-1
-Insights
-Settings
-We found potential security vulnerabilities in your dependencies.
-Only the owner of this repository can see this message.
-
-gravity-ts/src/connect.ts
-@LucasHRTeam
-LucasHRTeam refactor: changing types from connection
- 1 contributor
-13 lines (10 sloc)  327 Bytes
 import makeWASocket, { useSingleFileAuthState } from '@adiwajshing/baileys'
 
 import pino from 'pino'
@@ -34,15 +11,3 @@ const sock = makeWASocket({
     })
     
 export { sock }
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
