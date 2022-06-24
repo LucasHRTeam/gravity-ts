2a9ea2b6c5221c47e343e25b4b90f25045aa9124
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-06-24 07:46:19 -0400

    Delete src/events directory

--- src/events/creds.update/index.ts
+++
@@ -1,8 +0,0 @@
-import { useSingleFileAuthState } from '@adiwajshing/baileys'
-import { sock } from '../../connect'
-
-const { saveState } = useSingleFileAuthState('./auth.json')
-const creds = (): any => sock.ev.on('creds.update', saveState)
-
-export { creds }
-
--- src/events/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./connection.update"
-export * from "./creds.update"
-export * from "./messages.upsert"
