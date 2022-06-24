f0dc33b6217c7046c92277eefd18d16407292f91
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-06-24 08:01:36 -0400

    Create index.ts

---
+++ src/main/adapters/baileys/events/creds.update/index.ts
@@ -0,0 +1,7 @@
+import { useSingleFileAuthState } from '@adiwajshing/baileys'
+import { sock } from '../../connect'
+
+const { saveState } = useSingleFileAuthState('./auth.json')
+const creds = (): any => sock.ev.on('creds.update', saveState)
+
+export { creds }
