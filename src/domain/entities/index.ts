d48621a9ca07ea175d3b1261017d16d5b7807d74
Lucas Santos Da Hora <89528999+LucasHRTeam@users.noreply.github.com>
2022-06-24 11:31:59 -0400

    Update index.ts

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,1 +1,12 @@
+import { ISendMessage } from '../../usecase'
 
+class DomainBot {
+  constructor(private readonly sendMessage: ISendMessage) {}
+  async sendMessage(from: string, idWa: string, message: any, ...aditionalParams: any[]): Promise<any> => {
+  try {
+      return this.sendMessage
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
